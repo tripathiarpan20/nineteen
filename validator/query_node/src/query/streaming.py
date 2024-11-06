659ea59152058a6c9887cd50da8c2f2bc1bd2676
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-11-06 04:08:41 +0100

    test

--- validator/query_node/src/query/streaming.py
+++ validator/query_node/src/query/streaming.py
@@ -201,7 +201,7 @@ async def query_node_stream(config: Config, contender: Contender, node: Node, pa
     assert node.symmetric_key_uuid is not None
 
     return client.make_streamed_post(
-        httpx_client=config.httpx_client,
+        httpx_client=httpx.AsyncClient(),
         server_address=address,
         keypair=config.keypair,
         validator_ss58_address=config.ss58_address,
